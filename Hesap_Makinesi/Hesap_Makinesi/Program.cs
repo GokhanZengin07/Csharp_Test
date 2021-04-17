35515ba3f175cec7904aa1df7deb67621bbb8470
GokhanZengin07 <62075674+GokhanZengin07@users.noreply.github.com>
2021-04-17 09:47:07 +0300

    Hesap_Makinesi

---
+++ Hesap_Makinesi/Hesap_Makinesi/Program.cs
@@ -0,0 +1,57 @@
+﻿
+using System;
+
+namespace Odev16042021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+
+        Start:
+            try
+            {
+
+                int sayi1 = 0;
+                int sayi2 = 0;
+
+                while (sayi1 < 1 | sayi1 > 100)
+                {
+                    Console.WriteLine("Lutfen sayi 1`i istenilen 1-100 arasinda giriniz");
+
+                    int v = int.Parse(Console.ReadLine());
+                    sayi1 = v;
+
+                }
+
+                while (sayi2 < 1 | sayi2 > 100)
+                {
+                    Console.WriteLine("Lutfen sayi 2`i istenilen 1-100 arasinda giriniz");
+
+                    int x = int.Parse(Console.ReadLine());
+                    sayi2 = x;
+
+                }
+
+                Console.WriteLine("Sayi1 {0} ve Sayi2 {1}`nin toplami {2}", sayi1, sayi2, sayi1 + sayi2);
+
+            }
+
+
+
+
+
+            catch (System.Exception a)
+            {
+                Console.WriteLine("Sayi girmediniz lutfen sayi giriniz");
+                goto Start;
+            }
+
+        }
+    }
+}
