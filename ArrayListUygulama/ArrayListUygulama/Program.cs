37177f9c98e51ae46467ed428095a225b6c9841c
GokhanZengin07 <62075674+GokhanZengin07@users.noreply.github.com>
2021-05-02 12:02:28 +0300

    ArraySiralama

---
+++ ArrayListUygulama/ArrayListUygulama/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ArrayListUygulama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList adSoyad = new ArrayList();
+            adSoyad.Add("Gokhan Zengin");
+            adSoyad.Add("Efe Mavi");
+            adSoyad.Add("Mehmet Zengin");
+
+            adSoyad.Remove("Gokhan Zengin");
+        }
+    }
+}
--- ArraySiralama/ArraySiralama/Program.cs
+++ ArraySiralama/ArraySiralama/Program.cs
@@ -8,8 +8,16 @@ namespace ArraySiralama
         {
             int[] dizi = { 10, 5, 7, 9, 15, 3, 8, 1, 6 };
             int[] siraliDizi = new int[dizi.Length];
-            
+            int g;
+
             // Siralama isleminden once:
+            //Array.Sort(dizi);
+            //Console.WriteLine(dizi);
+            Array.Sort(dizi);
+            
+            Array.Reverse(dizi);
+            Console.WriteLine(dizi[0] + "-") ;
+            
             
             foreach (var item in dizi )
             {
@@ -26,8 +34,37 @@ namespace ArraySiralama
                 
             }
             Console.WriteLine("\nDizi boyutu " + dizi.Length);
-            Console.ReadLine();
+            
 
+            for (int i = 0; i < dizi.Length; i++)
+            {
+                for (int j = 0; j < dizi.Length; j++)
+                {
+                    if (dizi[i]>dizi[j])
+                    {
+                        g = dizi[j];
+                        dizi[j] = dizi[i];
+                        dizi[i] = g;
+
+                    }
+                }
+            }
+            
+            foreach (var item in dizi) 
+            {
+               
+                    if (item == dizi[(dizi.Length) - 1])
+                    {
+                        Console.Write(item);
+                    }
+                    else
+                    {
+                        Console.Write(item + "-");
+                    }
+
+                }
+            }
+        
         }
     }
-}
+
